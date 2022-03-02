89c7890928ee00802bf9e595e3ba0caa683bf6a0
Jhan Jamerly Castro Vargas - Ceiba Software <jhan.castro@ceiba.com.co>
2022-03-02 00:40:35 -0500

    sonar

---
+++ src/app/core/interceptor/auth-interceptor.spec.ts
@@ -0,0 +1,23 @@
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthInterceptor } from './auth-interceptor';
+
+
+describe('AuthInterceptor', () => {
+  let service: AuthInterceptor;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule,
+        RouterTestingModule],
+        providers:[AuthInterceptor]
+    });
+    service = TestBed.inject(AuthInterceptor);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});

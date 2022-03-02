2f6a16178316a9544a63f6739ef42d22552c92dd
Jhan Jamerly Castro Vargas - Ceiba Software <jhan.castro@ceiba.com.co>
2022-03-01 23:18:50 -0500

    collect component test

--- src/app/feature/locker/components/collect/collect.component.spec.ts
+++ src/app/feature/locker/components/collect/collect.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { CollectComponent } from './collect.component';
 
@@ -8,7 +9,11 @@ describe('CollectComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CollectComponent ]
+      declarations: [ CollectComponent ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+        
+    ]
     })
     .compileComponents();
   });
--- src/app/feature/locker/components/save/save.component.spec.ts
+++ src/app/feature/locker/components/save/save.component.spec.ts
@@ -1,7 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FilterLockerByTypePipe } from '@locker/shared/pipes/filter-locker-by-type.pipe';
 import { SaveComponent } from './save.component';
+import { MatDialog } from '@angular/material/dialog';
 
 describe('SaveComponent', () => {
   let component: SaveComponent;
@@ -10,7 +12,11 @@ describe('SaveComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports:[HttpClientTestingModule],
-      declarations: [ SaveComponent,FilterLockerByTypePipe ]
+      declarations: [ SaveComponent,FilterLockerByTypePipe ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialog, useValue: {} }
+    ]
     })
     .compileComponents();
   });

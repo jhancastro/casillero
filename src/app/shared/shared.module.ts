8490c152b7ee257d77499b4289bd77f809e2010e
Jhan Jamerly Castro Vargas - Ceiba Software <jhan.castro@ceiba.com.co>
2022-03-04 01:35:36 -0500

    ajustes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,20 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ErrorCamposPlantillaComponent } from './directivas/error-campos/componente/error-campos-plantilla.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { TrackByPipe } from './pipe/track-by.pipe';
 
 @NgModule({
   declarations: [
-    ErrorCamposPlantillaComponent,
-    TrackByPipe
+    ErrorCamposPlantillaComponent    
   ],
   imports: [ReactiveFormsModule, FormsModule],
   exports: [
     CommonModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule,
-    TrackByPipe
+    FormsModule    
   ]
 })
 export class SharedModule { }

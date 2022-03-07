0b0dc7dbbe4c581feabdcd25f63d319f54759efa
Jhan Jamerly Castro Vargas - Ceiba Software <jhan.castro@ceiba.com.co>
2022-03-06 23:55:35 -0500

    modificacion login.component

--- src/app/feature/login/login/login.component.spec.ts
+++ src/app/feature/login/login/login.component.spec.ts
@@ -109,4 +109,22 @@ describe('LoginComponent', () => {
     expect(component.error).toBe('User/password incorrect');
   });
 
+  it('Debe de validar login fallido desde loginUsers - sin token', () => {
+    spyOn(component.loginService,'loginUser').and.callFake(()=>of(
+        {
+            id: 1,
+            email: 'jhancas@gmail.com',
+            password: '123456789',
+            token: undefined
+        }
+     ));
+    
+    const email = component.formLogin.controls['email']; 
+    const password = component.formLogin.controls['password'];
+    email.setValue('jhancas@gmail.com');
+    password.setValue('12345678');
+    component.login()
+    expect(component.error).toBe('User/password incorrect');
+  });
+
 });

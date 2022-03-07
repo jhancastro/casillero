9e2029bf1dbb1f440ed152c60b1ba071c315b76c
Jhan Jamerly Castro Vargas - Ceiba Software <jhan.castro@ceiba.com.co>
2022-03-06 22:19:34 -0500

    modificacion JenkinsFile

--- e2e/src/page/navbar/navbar.po.ts
+++ e2e/src/page/navbar/navbar.po.ts
@@ -1,8 +1,7 @@
 import { by, element } from 'protractor';
 
 export class NavbarPage {
-    linkHome = element(by.xpath('/html/body/app-root/app-navbar/nav/a[1]'));
-    linkProducto = element(by.xpath('/html/body/app-root/app-navbar/nav/a[2]'));
+    linkProducto = element(by.xpath('/html/body/app-root/app-home/app-navbar/nav/a[1]'));
 
     async clickBotonProductos() {
         await this.linkProducto.click();
